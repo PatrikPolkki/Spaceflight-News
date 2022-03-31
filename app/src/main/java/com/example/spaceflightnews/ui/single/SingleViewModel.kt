8a20dc8dffc159b578ab506c7199a8acd6f7d792
patep <patrik.polkki@metropolia.fi>
2022-03-31 22:24:17 +0300

    first version of UI using material card view. implemented Glide for images.

--- app/src/main/java/com/example/spaceflightnews/ui/single/SingleViewModel.kt
+++ app/src/main/java/com/example/spaceflightnews/ui/single/SingleViewModel.kt
@@ -3,9 +3,16 @@ package com.example.spaceflightnews.ui.single
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.spaceflightnews.data.model.Article
+import com.example.spaceflightnews.data.model.Events
+import com.example.spaceflightnews.data.model.Launches
+import com.example.spaceflightnews.repository.SpaceflightRepository
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class SingleViewModel : ViewModel() {
+    private val repository = SpaceflightRepository
 
     private val _singleArticle: MutableLiveData<Article> by lazy {
         MutableLiveData<Article>()
@@ -16,5 +23,25 @@ class SingleViewModel : ViewModel() {
 
     fun addArticle(article: Article) {
         _singleArticle.value = article
+    }
+
+    fun getLaunches(launches: List<Launches>) {
+        viewModelScope.launch {
+            for (l in launches) {
+                val launch = async {
+
+                }
+            }
+        }
+    }
+
+    fun getEvents(event: List<Events>) {
+        viewModelScope.launch {
+            for (e in event) {
+                val event = async {
+                }
+            }
+
+        }
     }
 }
