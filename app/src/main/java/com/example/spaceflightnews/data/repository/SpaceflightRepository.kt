e1441e169a4e7f5f5ecb5eda746e4700ee7f410c
patep <patrik.polkki@metropolia.fi>
2022-04-03 15:24:03 +0300

    changed suspend functions to normal functions which equals flow rather than returns flow. Should be same outcome

--- app/src/main/java/com/example/spaceflightnews/data/repository/SpaceflightRepository.kt
+++ app/src/main/java/com/example/spaceflightnews/data/repository/SpaceflightRepository.kt
@@ -15,43 +15,39 @@ object SpaceflightRepository {
     private val retrofitInstance = RetrofitInstance
     private val serviceCall = retrofitInstance.service
 
-    suspend fun getArticles(limit: Int): Flow<Recourse<List<Article>>> {
-        return flow {
-            try {
-                emit(Recourse.Loading())
-                val articleList = serviceCall.getArticles(limit)
-                emit(Recourse.Success(articleList))
-            } catch (e: HttpException) {
-                emit(Recourse.Error(e.localizedMessage ?: "An unexpected error."))
-            } catch (e: IOException) {
-                emit(Recourse.Error("Couldn't reach server."))
-            }
-        }.flowOn(Dispatchers.IO)
-    }
+    fun getArticles(limit: Int): Flow<Recourse<List<Article>>> = flow {
+        try {
+            emit(Recourse.Loading())
+            val articleList = serviceCall.getArticles(limit)
+            emit(Recourse.Success(articleList))
+        } catch (e: HttpException) {
+            emit(Recourse.Error(e.localizedMessage ?: "An unexpected error."))
+        } catch (e: IOException) {
+            emit(Recourse.Error("Couldn't reach server."))
+        }
+    }.flowOn(Dispatchers.IO)
 
-    suspend fun getEvents(id: Long): Flow<List<Article>> {
-        return flow {
-            try {
-                val eventList = serviceCall.getEvents(id)
-                emit(eventList)
-            } catch (e: HttpException) {
-                e.message?.let { Log.e("HttpException", it) }
-            } catch (e: IOException) {
-                e.message?.let { Log.e("IOException", it) }
-            }
-        }.flowOn(Dispatchers.IO)
-    }
+    fun getEvents(id: Long): Flow<List<Article>> = flow {
+        try {
+            val eventList = serviceCall.getEvents(id)
+            emit(eventList)
+        } catch (e: HttpException) {
+            e.message?.let { Log.e("HttpException", it) }
+        } catch (e: IOException) {
+            e.message?.let { Log.e("IOException", it) }
+        }
+    }.flowOn(Dispatchers.IO)
 
-    suspend fun getLaunches(id: String): Flow<List<Article>> {
-        return flow {
-            try {
-                val launchList = serviceCall.getLaunches(id)
-                emit(launchList)
-            } catch (e: HttpException) {
-                e.message?.let { Log.e("HttpException", it) }
-            } catch (e: IOException) {
-                e.message?.let { Log.e("IOException", it) }
-            }
-        }.flowOn(Dispatchers.IO)
-    }
+
+    fun getLaunches(id: String): Flow<List<Article>> = flow {
+        try {
+            val launchList = serviceCall.getLaunches(id)
+            emit(launchList)
+        } catch (e: HttpException) {
+            e.message?.let { Log.e("HttpException", it) }
+        } catch (e: IOException) {
+            e.message?.let { Log.e("IOException", it) }
+        }
+    }.flowOn(Dispatchers.IO)
+
 }
